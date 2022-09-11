3d7b2834607c652cba8cb400d494373ee33f67bc
litvinovDanil112011 <danil112011@mail.ru>
2022-09-11 16:25:53 +0300

    added table 2 screen transition between them.

---
+++ DL-HW25/Module/InfoPersonViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InfoPersonViewController.swift
+//  DL-HW25
+//
+//  Created by Daniil Litvinov on 11.09.2022.
+//
+
+import UIKit
+
+class InfoPersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayuot()
+        setupView()
+        view.backgroundColor = .orange
+        
+    }
+    
+    private func setupLayuot() {
+        
+    }
+    
+    private func setupView() {
+        
+        }
+}
